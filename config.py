7f949529bc717f4ca8180f4704f8241db2fe39b9
tamilarasumk731 <tamilarasu.mk731@gmail.com>
2020-04-21 00:44:45 +0530

    DB config modified

--- config.py
+++ config.py
@@ -15,10 +15,7 @@ def get_env_variable(name):
     raise Exception(message)
 
 # the values of those depend on your setup
-POSTGRES_URL = get_env_variable("POSTGRES_URL")
-POSTGRES_USER = get_env_variable("POSTGRES_USER")
-POSTGRES_PW = get_env_variable("POSTGRES_PW")
-POSTGRES_DB = get_env_variable("POSTGRES_DB")
+
 
 class Config(object):
   DEBUG = False
@@ -28,17 +25,26 @@ class Config(object):
 
 
 class ProductionConfig(Config):
-  DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
-  SQLALCHEMY_DATABASE_URI = DB_URL
+  SQLALCHEMY_DATABASE_URI = get_env_variable['DATABASE_URL']
   DEBUG = False
 
 
 class StagingConfig(Config):
+  POSTGRES_URL = get_env_variable("POSTGRES_URL")
+  POSTGRES_USER = get_env_variable("POSTGRES_USER")
+  POSTGRES_PW = get_env_variable("POSTGRES_PW")
+  POSTGRES_DB = get_env_variable("POSTGRES_DB")
+  DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+  SQLALCHEMY_DATABASE_URI = DB_URL
   DEVELOPMENT = True
   DEBUG = True
 
 
 class DevelopmentConfig(Config):
+  POSTGRES_URL = get_env_variable("POSTGRES_URL")
+  POSTGRES_USER = get_env_variable("POSTGRES_USER")
+  POSTGRES_PW = get_env_variable("POSTGRES_PW")
+  POSTGRES_DB = get_env_variable("POSTGRES_DB")
   DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
   SQLALCHEMY_DATABASE_URI = DB_URL
   DEVELOPMENT = True
@@ -46,4 +52,10 @@ class DevelopmentConfig(Config):
 
 
 class TestingConfig(Config):
+  POSTGRES_URL = get_env_variable("POSTGRES_URL")
+  POSTGRES_USER = get_env_variable("POSTGRES_USER")
+  POSTGRES_PW = get_env_variable("POSTGRES_PW")
+  POSTGRES_DB = get_env_variable("POSTGRES_DB")
+  DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+  SQLALCHEMY_DATABASE_URI = DB_URL
   TESTING = True
