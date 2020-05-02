3915bf03e6d26427d47feced549d0abdda968b2b
VigneshKarthigeyan <vickykarthy123@gmail.com>
2020-05-02 13:21:53 +0530

    An API to get Cumulative sum of PRs,Commits,Contributors of Team-Tomato

---
+++ Git.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from github import Github
+import os,requests,json
+
+app = Flask(__name__)
+g = Github()
+@app.route('/get_total')
+def total():
+    commits = 0
+    pulls = 0
+    conts = 0
+    a = requests.get("https://api.github.com/users/Team-Tomato/repos")
+    y = json.loads(a.text)
+    for i in y:
+        repository = "Team-Tomato/{}".format(i["name"])
+        repo = g.get_repo(repository)
+        pulls += repo.get_pulls().totalCount
+        commits += repo.get_commits().totalCount
+        conts += repo.get_contributors().totalCount
+    lst = []
+    lst.append(dict([("Name: ", "Team-Tomato"), ("Pull Requests: ", pulls), ("Commits: ", commits), ("Contributors: ", conts)]))
+    return jsonify(lst)
+
+if __name__ == '__main__':
+    app.run()
